2f25ee2b395c7610da2afd6191b78b0fb947fd61
Felipe Santos <fsantosmg@gmail.com>
2025-03-23 11:10:39 -0300

    Update script_ubuntu.sh
    instalação de dependencias

--- script_ubuntu.sh
+++ script_ubuntu.sh
@@ -6,7 +6,10 @@ sudo apt update && sudo apt upgrade -y
 
 # Instala pacotes essenciais
 echo "Instalando pacotes essenciais..."
-sudo apt install -y curl wget git unzip tar build-essential
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+  libffi-dev liblzma-dev libgdbm-dev libnss3-dev libdb-dev 
 
 # Instala o Docker
 echo "Instalando o Docker..."
