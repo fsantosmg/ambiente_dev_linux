3838c266aeddd4f98b6ea5d0931f4dddbaa8395e
Felipe Santos <fsantosmg@gmail.com>
2025-04-06 09:51:25 -0300

    Update script_ubuntu.sh
    lib para AppImage

--- script_ubuntu.sh
+++ script_ubuntu.sh
@@ -9,7 +9,7 @@ echo "Instalando pacotes essenciais..."
 sudo apt install -y make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-  libffi-dev liblzma-dev libgdbm-dev libnss3-dev libdb-dev 
+  libffi-dev liblzma-dev libgdbm-dev libnss3-dev libdb-dev libfuse2
 
 # Instala o Docker
 echo "Instalando o Docker..."
